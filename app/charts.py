41e5fe18bbf4248111e586569f570d709e4cf7f6
juandiegomade <juandiegomade@gmail.com>
2022-12-25 20:53:01 -0600

    added docker files

--- app/charts.py
+++ app/charts.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(name, labels, values):   
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefigf(f'./imgs/{name}.png')
+  plt.savefig(f'./imgs/{name}.png')
   plt.close()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig('pie.png')
+  plt.savefig('chart_pie.png')
   plt.close()
 
 if __name__ == '__main__':
